5c958b6fdec6b8d7a716cd5ba8878544f36e57fd
Pesshoo <76278967+Pesshoo@users.noreply.github.com>
2024-06-01 15:41:52 +0300

    Az moga tuk i sega - 4394

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 // Продължи с твоя vanilla JavaScript код тук.
+const ham = document.querySelector(".hamburger")
+const menu = document.querySelector(".off-screen-menu")
+
+ham.addEventListener("click", ()=> {
+    menu.classList.toggle("active")
+    ham.classList.toggle("active")
+})
